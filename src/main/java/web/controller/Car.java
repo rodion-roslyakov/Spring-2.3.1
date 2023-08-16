56dd6e182376a7a001ea494c933af7fbbcefedcb
Rodion <roslyakov_rodion@mail.ru>
2023-08-16 11:00:36 +0300

    Задание выполнено!

---
+++ src/main/java/web/controller/Car.java
@@ -0,0 +1,68 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Controller
+public class Car {
+
+    private String model;
+    private String series;
+    private int year;
+
+    public Car(String model, String series, int year) {
+        this.model = model;
+        this.series = series;
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series='" + series + '\'' +
+                ", year=" + year +
+                '}';
+    }
+
+    public Car() {
+    }
+
+    @GetMapping(value = "/cars")
+    public String printCars(@RequestParam(value = "count", required = false) int count,
+                            ModelMap model) {
+
+        Car car1 = new Car("Geely", "Coolray", 2023);
+        Car car2 = new Car("Geely", "Monjaro", 2023);
+        Car car3 = new Car("Geely", "Tugella", 2023);
+        Car car4 = new Car("Porsche", "911turboS", 2023);
+        Car car5 = new Car("BMW", "XM", 2023);
+
+        List<Car> cars = new ArrayList<>();
+        cars.add(car1);
+        cars.add(car2);
+        cars.add(car3);
+        cars.add(car4);
+        cars.add(car5);
+
+        List<String> messages = new ArrayList<>();
+        if (count > 0 && count <= 5) {
+            for (int i = 0; i < count; i++) {
+                messages.add(cars.get(i).toString());
+            }
+        }
+        if(count>5){
+            for (Car car :cars){
+                messages.add(car.toString());
+            }
+        }
+        model.addAttribute("messages", messages);
+        return "cars";
+    }
+}
