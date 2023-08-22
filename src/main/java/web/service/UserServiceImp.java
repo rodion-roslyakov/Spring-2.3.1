4770dca0aab80eca57fbf4114f136c8f62847be3
Rodion <roslyakov_rodion@mail.ru>
2023-08-22 14:10:41 +0300

    Задание выполнено!

--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -20,33 +20,26 @@ public class UserServiceImp implements UserService {
     }
 
     public List<User> index() {
-//        return userRepository.findAll();
         return userDao.index();
     }
 
 
     public User show(int id) {
-//        Optional<User> findUser = userRepository.findById(id);
-//        return findUser.orElse(null);
         return userDao.show(id);
     }
 
     @Transactional
     public void update(int id, User updateUser) {
-//        updateUser.setId(id);
-//        userRepository.save(updateUser);
         userDao.update(id, updateUser);
     }
 
     @Transactional
     public void delete(int id) {
-//        userRepository.deleteById(id);
         userDao.delete(id);
     }
 
     @Transactional
     public void save(User user) {
-//        userRepository.save(user);
         userDao.save(user);
     }
 }
